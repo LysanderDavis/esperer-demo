6cc746abe50428395fe5775ea8ec448fd5ae1eb1
Laziz <104445440+Emotion321@users.noreply.github.com>
2025-05-26 17:27:03 +0500

    some link changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,27 @@
 	import Footer from '$lib/components/layout/Footer.svelte';
 </script>
 
-<main class="min-h-screen bg-[your-color">
+<svelte:head>
+	<title>Espérer</title>
+	<meta property="og:title" content="Espérer" />
+	<meta property="og:url" content="https://esperer.netlify.app" />
+	<meta
+		property="og:image"
+		content="https://esperer.netlify.app/esperer-high-resolution-logo-transparent.png"
+	/>
+	<meta property="og:type" content="website" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="description" content="Mapping a Sustainable Future" />
+	<meta property="og:description" content="Join Us for Global Impact" />
+	<meta property="og:site_name" content="Espérer" />
+	<meta name="twitter:title" content="Espérer" />
+	<meta
+		name="twitter:image"
+		content="https://esperer.netlify.app/esperer-high-resolution-logo-transparent.png"
+	/>
+</svelte:head>
+
+<main class="min-h-screen bg-[your-color]">
 	<Header />
 	<slot />
 	<Footer />
