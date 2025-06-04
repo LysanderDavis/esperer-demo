8df907476e8c6135bb30243f912832622528a842
Laziz <104445440+Emotion321@users.noreply.github.com>
2025-06-04 13:45:28 +0500

    Initiatives added

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -33,6 +33,7 @@
 			<div class="hidden items-center space-x-8 text-white lg:flex">
 				<a href="/about" class="transition-colors hover:underline">About</a>
 				<a href="/global-impact" class="transition-colors hover:underline">Global Impact</a>
+				<a href="/initiatives" class="transition-colors hover:underline">Initiatives</a>
 				<a href="mailto:esperer8@substack.com" class="transition-colors hover:underline">Contact</a
 				>
 				<a
