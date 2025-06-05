52116c32cffab7e500624294f1ed8f6d0810ffda
Laziz <104445440+Emotion321@users.noreply.github.com>
2025-06-05 09:07:39 +0500

    will retry some stuff

--- src/lib/supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-import { createBrowserClient } from '@supabase/ssr';
-import { env } from '$env/dynamic/public';
-
-export const supabase = createBrowserClient(
-	env.PUBLIC_SUPABASE_URL!,
-	env.PUBLIC_SUPABASE_ANON_KEY!
-);
